eed6ac8ecbe4e061b0bce6a66169f968e2aa959d
Aditya Dave <aditya.dave2017@gmail.com>
2021-10-08 01:58:22 -0700

    implmented f1 score and distance functions

--- 01_startercode_knn/utils.py
+++ 01_startercode_knn/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 from knn import KNN
 
 ############################################################################
-# DO NOT MODIFY CODES ABOVE 
+# DO NOT MODIFY CODES ABOVE
 ############################################################################
 
 
@@ -15,12 +15,25 @@ def f1_score(real_labels, predicted_labels):
     :return: float
     """
     assert len(real_labels) == len(predicted_labels)
-    raise NotImplementedError
+
+    # Convert to numpy array
+    arr_real_labels = np.array(real_labels)
+    arr_predicted_labels = np.array(predicted_labels)
+
+    # Get true positive, false positive and false negative
+    true_positive = np.sum(
+        arr_predicted_labels[arr_predicted_labels == 1][arr_real_labels == 1])
+    false_positive = np.sum(
+        arr_predicted_labels[arr_predicted_labels == 1][arr_real_labels == 0])
+    false_negative = np.sum(
+        arr_predicted_labels[arr_predicted_labels == 0][arr_real_labels == 1])
+
+    # Calculate F1 score and return
+    return true_positive / (true_positive + ((false_positive + false_negative) / 2))
 
 
 class Distances:
     @staticmethod
-    # TODO
     def minkowski_distance(point1, point2):
         """
         Minkowski distance is the generalized version of Euclidean Distance
@@ -31,28 +44,33 @@ class Distances:
         :param point2: List[float]
         :return: float
         """
-        raise NotImplementedError
+        return np.cbrt(np.sum(np.power(np.array(point1) - np.array(point2), 3)))
 
     @staticmethod
-    # TODO
     def euclidean_distance(point1, point2):
         """
         :param point1: List[float]
         :param point2: List[float]
         :return: float
         """
-        raise NotImplementedError
+        return np.sqrt(np.sum(np.power(np.array(point1) - np.array(point2), 2)))
 
     @staticmethod
-    # TODO
     def cosine_similarity_distance(point1, point2):
         """
        :param point1: List[float]
        :param point2: List[float]
        :return: float
        """
-        raise NotImplementedError
+        arr_point1 = np.array(point1)
+        arr_point2 = np.array(point2)
+        ed_point1 = np.sqrt(np.sum(np.power(arr_point1, 2)))
+        ed_point2 = np.sqrt(np.sum(np.power(arr_point2, 2)))
 
+        if ed_point1 == 0 or ed_point2 == 0:
+            return 1
+        else:
+            return 1 - ((np.dot(arr_point1, arr_point2))/ed_point1 * ed_point2)
 
 
 class HyperparameterTuner:
@@ -66,7 +84,7 @@ class HyperparameterTuner:
     def tuning_without_scaling(self, distance_funcs, x_train, y_train, x_val, y_val):
         """
         In this part, you need to try different distance functions you implemented in part 1.1 and different values of k (among 1, 3, 5, ... , 29), and find the best model with the highest f1-score on the given validation set.
-		
+
         :param distance_funcs: dictionary of distance functions (key is the function name, value is the function) you need to try to calculate the distance. Make sure you loop over all distance functions for each k value.
         :param x_train: List[List[int]] training data set to train your KNN model
         :param y_train: List[int] training labels to train your KNN model
@@ -79,10 +97,10 @@ class HyperparameterTuner:
 
         NOTE: When there is a tie, choose the model based on the following priorities:
         First check the distance function:  euclidean > Minkowski > cosine_dist 
-		(this will also be the insertion order in "distance_funcs", to make things easier).
+                (this will also be the insertion order in "distance_funcs", to make things easier).
         For the same distance function, further break tie by prioritizing a smaller k.
         """
-        
+
         # You need to assign the final values to these variables
         self.best_k = None
         self.best_distance_function = None
@@ -93,7 +111,7 @@ class HyperparameterTuner:
     def tuning_with_scaling(self, distance_funcs, scaling_classes, x_train, y_train, x_val, y_val):
         """
         This part is the same as "tuning_without_scaling", except that you also need to try two different scalers implemented in Part 1.3. More specifically, before passing the training and validation data to KNN model, apply the scalers in scaling_classes to both of them. 
-		
+
         :param distance_funcs: dictionary of distance functions (key is the function name, value is the function) you need to try to calculate the distance. Make sure you loop over all distance functions for each k value.
         :param scaling_classes: dictionary of scalers (key is the scaler name, value is the scaler class) you need to try to normalize your data
         :param x_train: List[List[int]] training data set to train your KNN model
@@ -102,11 +120,11 @@ class HyperparameterTuner:
         :param y_val: List[int] validation labels
 
         Find the best k, distance_function (its name), scaler (its name), and model (an instance of KNN), and assign them to self.best_k, self.best_distance_function, best_scaler, and self.best_model respectively.
-        
+
         NOTE: When there is a tie, choose the model based on the following priorities:
         First check scaler, prioritizing "min_max_scale" over "normalize" (which will also be the insertion order of scaling_classes). Then follow the same rule as in "tuning_without_scaling".
         """
-        
+
         # You need to assign the final values to these variables
         self.best_k = None
         self.best_distance_function = None
@@ -141,15 +159,15 @@ class MinMaxScaler:
     # TODO: min-max normalize data
     def __call__(self, features):
         """
-		For each feature, normalize it linearly so that its value is between 0 and 1 across all samples.
+                For each feature, normalize it linearly so that its value is between 0 and 1 across all samples.
         For example, if the input features are [[2, -1], [-1, 5], [0, 0]],
         the output should be [[1, 0], [0, 1], [0.333333, 0.16667]].
-		This is because: take the first feature for example, which has values 2, -1, and 0 across the three samples.
-		The minimum value of this feature is thus min=-1, while the maximum value is max=2.
-		So the new feature value for each sample can be computed by: new_value = (old_value - min)/(max-min),
-		leading to 1, 0, and 0.333333.
-		If max happens to be same as min, set all new values to be zero for this feature.
-		(For further reference, see https://en.wikipedia.org/wiki/Feature_scaling.)
+                This is because: take the first feature for example, which has values 2, -1, and 0 across the three samples.
+                The minimum value of this feature is thus min=-1, while the maximum value is max=2.
+                So the new feature value for each sample can be computed by: new_value = (old_value - min)/(max-min),
+                leading to 1, 0, and 0.333333.
+                If max happens to be same as min, set all new values to be zero for this feature.
+                (For further reference, see https://en.wikipedia.org/wiki/Feature_scaling.)
 
         :param features: List[List[float]]
         :return: List[List[float]]
